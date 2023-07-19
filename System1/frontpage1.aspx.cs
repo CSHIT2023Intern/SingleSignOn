61397c9be1c9e039f27a93591bdcee67946e9a92
Jessica-0112 <ica3431652@gmail.com>
2023-07-19 17:29:14 +0800

    SSO

---
+++ System1/frontpage1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web;
+using static SingleSignOn.Login;
+
+namespace SingleSignOn
+{
+    public partial class frontpage1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+            if (Session["LoggedIn"] != null && (bool)Session["LoggedIn"])
+            {
+                if (Request.Cookies[TokenManager.TokenCookieName] != null)
+                {
+                    title.Text = "Welcome to System1！";
+                    btnLogout.Visible = true;
+                }
+                else
+                {
+                    Response.Redirect("login1.aspx");
+                }
+            }
+            else
+            {
+                Response.Redirect("login1.aspx");
+            }
+        }
+
+        protected void Btn_Logout_Click(object sender, EventArgs e)
+        {
+            TokenManager.CentralizedLogout();
+
+            Session["LoggedIn"] = false; // 清除使用者登入狀態
+
+            Response.Redirect("login1.aspx");
+        }
+    }
+}
---
+++ System2/frontpage2.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web;
+using static SingleSignOn.Login;
+
+namespace SingleSignOn
+{
+    public partial class frontpage2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["LoggedIn"] != null && (bool)Session["LoggedIn"])
+            {
+                if (Request.Cookies[TokenManager.TokenCookieName] != null)
+                {
+                    title.Text = "Welcome to System2！";
+                    btnLogout.Visible = true;
+                }
+                else
+                {
+                    Response.Redirect("login2.aspx");
+                }
+            }
+            else
+            {
+                Response.Redirect("login2.aspx");
+            }
+        }
+
+        protected void Btn_Logout_Click(object sender, EventArgs e)
+        {
+            TokenManager.CentralizedLogout();
+
+            Session["LoggedIn"] = false; // 清除使用者登入狀態
+
+            Response.Redirect("login2.aspx");
+        }
+    }
+}
